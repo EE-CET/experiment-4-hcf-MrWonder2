832a9e13849cebb4dce6f78c0e87520b30e5991b
Bharath R <107768150+MrWonder2@users.noreply.github.com>
2026-03-16 21:50:12 +0530

    Completed

--- src/HCF.java
+++ src/HCF.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class HCF {
-    
-        // TODO: Read two integers and print their HCF
-    
+        public static int findHCF(int a, int b) {
+                while (b != 0) {
+                        int temp = b;
+                        b = a % b;
+                        a = temp;
+                }
+                return a;
+        }
+
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int num1 = sc.nextInt();
+                int num2 = sc.nextInt();
+                int hcf = findHCF(num1, num2);
+                System.out.println(hcf);
+                sc.close();
+        }
+
 }
